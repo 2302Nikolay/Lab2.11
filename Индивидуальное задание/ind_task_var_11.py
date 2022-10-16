dbe92e8811f77d9d7ff9b85f6595ff21b585f9cd
2302Nikolay <Malzewn@gmail.com>
2022-10-16 11:03:41 +0300

    Выполнил индивидуальное задание и добавил отчёт

---
+++ Индивидуальное задание/ind_task_var_11.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# _*_ coding: utf-8 _*_
+
+def f1():
+    def f2(x):
+        return x + 3
+
+    return f2
+
+
+if __name__ == '__main__':
+    k = int(input())
+    cnt = f1()
+    print(cnt(k))
